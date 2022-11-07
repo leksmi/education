995bc47c752c13a3d4597964b970e74f3d55aea7
leksmi <alexdotspb@gmail.com>
2022-11-07 11:31:01 +0300

    SelfEdu: Different changes

---
+++ pyneng/oop_1.py
@@ -0,0 +1,51 @@
+import time
+import telnetlib
+from getpass import
+
+
+class BaseTelnet:
+    def __init__(self, ip: str,
+                 username: str,
+                 password: str,
+                 prompt: str,
+                 short_sleep: int = 1,
+                 long_sleep: int = 5
+                 ):
+        print('__init__ base class')
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.prompt = prompt
+        self.short_sleep = short_sleep
+        self.long_sleep = long_sleep
+
+        self._telnet = telnetlib.Telnet(ip, timeout=7)
+        self._telnet.read_until(b'Username:')
+        self._telnet.write(username.encode('utf-8') + b'\n')
+        self._telnet.read_until(b'Password:')
+        self._telnet.write(password.encode('utf-8') + b'\n')
+        time.sleep(self.short_sleep)
+        self._telnet.read_very_eager()
+
+    def send_command(self, command: str):
+        self._telnet.write(command.encode('utf-8') + b'\n')
+        # time.sleep(self.long_sleep)
+        # output = self._telnet.read_very_eager().decode('utf-8')
+        output = self._read_until_prompt()
+        return output.replace('\r\n', '\n')
+
+    def _read_until_prompt(self):
+        output = self._telnet.read_until(self.prompt.encode('utf-8'))
+        return output.decode('utf-8')
+
+    def close(self):
+        '''
+        Close telnet connection.
+        :return: none
+        '''
+        self._telnet.close()
+
+
+if __name__ == '__main__':
+    with BaseTelnet(ip='10.252.135.34', username='nngs-sps', password=input('Password: '), prompt='>') as conn:
+        print(conn.send_command('sho clock'))
--- python_russia/args_kwargs.py
+++ python_russia/args_kwargs.py
@@ -3,20 +3,51 @@ a, b, c = [True, False, None]
 a, *b, c = 'ABCDEF'
 *a, b, c = 'ABCDEF'
 lst1 = [True, False, None]
+print(*lst1)
 
 
-def main(a, b, c):
+def test_func_for_nothing():
+    print('nothing')
+
+
+def main(a, b, /, c, *, d=None):
+    # / - слева от слэша это ОБЯЗАТЕЛЬНО позиционные аргументы
+    # * - после звезды это ОБЯЗАТЕЛЬНО ключевые аргументы
     print(f'{a=}')
     print(f'{b=}')
     print(f'{c=}')
 
 
+def my_print(a, *b):
+    print(type(a), a)
+    print(type(b), b)
+
+
+def my_print_args(*args):  # множество поз-х арг-в будет Упаковано в кортеж
+    for i in args:
+        print(i)
+
+
+def my_print_args_kwargs(*args, **kwargs):
+    print(f'args это {type(args)}', args)
+    print(f'kwargs это {type(kwargs)}', kwargs)
+    if kwargs:
+        for i in kwargs:
+            print(kwargs[i])
+
+
 if __name__ == '__main__':
-    main(a='Aa', c='Cc', b='Bb')
+    main('Aa', 'Bb', c='Cc')
     main(101, 'bB', c='cC')
     main(*lst1)
-
-def test_func_for_nothing():
-    print('nothing')
-
-print(*lst1)
+    my_print(101, 'H', 'e', 505, None)
+    my_print_args(11, 12, 13, 14, 15, 16, 17)
+    lst_2 = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+    my_print_args(lst_2)
+    print()
+    my_print_args(*lst_2)  # сначала будет распакован lst_2 и передан как множество поз-х арг-в
+    print()
+    my_print_args_kwargs(33, 44, 55, five= 5, seven=7)
+    dev = {'host': '192.168.1.11', 'platform': 'Cisco inc', 'username': 'cisco', 'password': '12345'}
+    my_print_args_kwargs(**dev)
+    my_print_args_kwargs()  # пустой вызов: будет пустой кортеж и пустой словарь
